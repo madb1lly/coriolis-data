f521070a56d42547d0cd6f1ed837b4e9c61ec695
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2016-11-27 00:06:51 +0000

    Added known special effects

--- modifications/index.js
+++ modifications/index.js
@@ -2,5 +2,6 @@ module.exports = {
   blueprints: require('./blueprints'),
   modifications: require('./modifications'),
   modifierActions: require('./modifierActions'),
+  specials: require('./specials'),
   validity: require('./validity')
 };
